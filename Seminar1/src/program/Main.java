7718ca37a8353e9ceae844d91f2c0d6b5e4236b0
Denisa <Denisa@DESKTOP-0T13ISG>
2021-02-25 17:18:22 +0200

    Adaugarea Claselor Pentru Gradina Zoologica

--- Seminar1/src/program/Main.java
+++ Seminar1/src/program/Main.java
@@ -1,10 +1,24 @@
 package program;
 
+import clase.Giraffe;
+import clase.Zoo;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Vrem sa ne creem o gradina zoologica
+		Zoo z = new Zoo();
+		
+		//Daca vrem sa ne adaugam un animal, trb. sa avem clasele concrete pentru animale
+		//Putem folosi clasele concrete peste tot unde am folosit clasele abstracte - CleanCode
+		Giraffe giraffe = new Giraffe("Girafa1");
+		Giraffe giraffe2 = new Giraffe("Girafa2");
 
+		z.addAnimal(giraffe);
+		z.addAnimal(giraffe2);
+		
+		z.feedAllAnimals();
 	}
 
 }
