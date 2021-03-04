1d2fdab28b0c36128ab61f124b0e52989d02e2a7
Denisa <Denisa@DESKTOP-0T13ISG>
2021-03-04 17:22:22 +0200

    Final Commit

---
+++ Seminar2/src/ro/ase/cts/clase/readers/AplicantReader.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.clase.readers;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import ro.ase.cts.clase.Aplicant;
+
+public abstract class AplicantReader {
+	protected String fileName;
+			
+	public AplicantReader(String fileName) {
+		super();
+		this.fileName = fileName;
+	}
+
+	public abstract List<Aplicant> readAplicanti() throws FileNotFoundException;
+		
+	
+}
--- Seminar2/src/ro/ase/cts/program/Program.java
+++ Seminar2/src/ro/ase/cts/program/Program.java
@@ -1,18 +1,25 @@
 package ro.ase.cts.program;
 
 import java.io.FileNotFoundException;
+
 import java.util.List;
 
-import ro.ase.cts.clase.Angajat;
-import ro.ase.cts.clase.Utils;
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.readers.AngajatReader;
+import ro.ase.cts.clase.readers.AplicantReader;
 
 public class Program {
 
+	public static List<Aplicant> citesteAplicanti(AplicantReader reader) throws FileNotFoundException{
+		return reader.readAplicanti();
+	}
+	
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
+			listaAngajati = citesteAplicanti( new AngajatReader("angajati.txt"));
+			
+			for(Aplicant angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
