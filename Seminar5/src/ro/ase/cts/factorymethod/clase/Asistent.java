3549b58fa37b57823e10874482177c0f2ea490b8
Denisa <Denisa@DESKTOP-0T13ISG>
2021-03-25 17:15:31 +0200

    Final Commit

---
+++ Seminar5/src/ro/ase/cts/factorymethod/clase/Asistent.java
@@ -0,0 +1,18 @@
+package ro.ase.cts.factorymethod.clase;
+
+public class Asistent extends PersonalSpital{
+
+	public Asistent(String nume) {
+		super(nume);
+		
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Asistent [nume=");
+		builder.append(nume);
+		builder.append("]");
+		return builder.toString();
+	}
+}
