488f22d6239e7b91548824ffc71dbd4f14feb12f
Denisa <Denisa@DESKTOP-0T13ISG>
2021-05-20 17:14:31 +0300

    Second commit

--- Seminar12/src/ro/ase/cts/clase/Student.java
+++ Seminar12/src/ro/ase/cts/clase/Student.java
@@ -30,7 +30,12 @@ public class Student {
     }
 
     public void adaugaNota(int nota){
+    	if(nota>=1 && nota<=10) {
         note.add(nota);
+    	}
+    	else {
+    		throw new IllegalArgumentException();
+    	}
     }
 
     public int getNota(int index){
@@ -48,15 +53,17 @@ public class Student {
         if(note.size()>0){
             return suma/note.size();
         }
-        return suma;
+        else {
+        	throw new IllegalArgumentException();
+        }
     }
     
     public boolean areRestante() {
     	for(int nota:note) {
     		if(nota<5) {
-    			return false;
+    			return true;
     		}
     	}
-    	return true;
+    	return false;
     }
 }
--- Seminar12/src/ro/ase/cts/teste/StudentTests.java
+++ Seminar12/src/ro/ase/cts/teste/StudentTests.java
@@ -47,6 +47,86 @@ public class StudentTests {
 		assertEquals(1,student.getNote().size());
 	}
 	
+	@Test
+	public void testCalculeazaMedie() {
+		Student student = new Student();
+		int nota1 = 5;
+		int nota2 = 9;
+		int nota3 = 10;
+		student.adaugaNota(nota1);
+		student.adaugaNota(nota2);
+		student.adaugaNota(nota3);
+		float medie = (nota1+nota2+nota3)/3.0f;
+		assertEquals(medie,student.calculeazaMedie(), 0.01);
+		//punem o marja de eroare cand lucram cu valori reale
+	}
 	
+	@Test
+	public void testCalculeazaMediePentruONota() {
+		Student student = new Student();
+		int nota1 = 5;
+		student.adaugaNota(nota1);
+		assertEquals(nota1,student.calculeazaMedie(), 0.01);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCalculeazaMediePentruNicioNota() {
+		Student student = new Student();
+		//assertEquals(0,student.calculeazaMedie(), 0.01);
+		student.calculeazaMedie();
+	}
+	
+	@Test
+	public void testAreRestanta() {
+		Student student = new Student();
+		int nota1 = 5;
+		int nota2 = 4;
+		int nota3 = 3;
+		student.adaugaNota(nota1);
+		student.adaugaNota(nota2);
+		student.adaugaNota(nota3);
+		assertTrue(student.areRestante());
+	}
+	
+	@Test
+	public void testNuAreRestanta() {
+		Student student = new Student();
+		int nota1 = 5;
+		int nota2 = 7;
+		int nota3 = 10;
+		student.adaugaNota(nota1);
+		student.adaugaNota(nota2);
+		student.adaugaNota(nota3);
+		assertFalse(student.areRestante());
+	}
+	
+	@Test
+	public void testGetNotaPozitieIncorecta() {
+		Student student = new Student();
+		int nota1 = 5;
+		int nota2 = 7;
+		int nota3 = 10;
+		student.adaugaNota(nota1);
+		student.adaugaNota(nota2);
+		student.adaugaNota(nota3);
+		//1
+		try {
+			//2
+			student.getNota(3);
+			//3
+			fail("metoda nu arunca exceptie");
+		}
+		catch(IndexOutOfBoundsException exceptie) {
+			//4
+		}
+		//5
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testAdaugaNotaIncorecta() {
+		Student student = new Student();
+		int nota1 = 11;
+		student.adaugaNota(nota1);
+	}
 	
 }
